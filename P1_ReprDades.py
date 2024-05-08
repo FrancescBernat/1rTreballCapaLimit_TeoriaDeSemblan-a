479e7058322cc2e7aa4ef8b6e2d17f1d4f6f2493
Francesc Bernat Bieri Tauler <francescbernatf@gmail.com>
2024-05-08 11:16:14 +0200

    Humitat especifica funcionant.

--- P1_ReprDades.py
+++ P1_ReprDades.py
@@ -13,10 +13,38 @@ import matplotlib.pyplot as plt
 
 import funcions as fun
 
+from importlib import reload
+
+reload(fun)
+
+
 arxiu = "Dades/DADES_26SETEMBRE2020.dat"
 
 colnames = ["time", "H", "LE", "ustar", "z_L", "w_speed",
             "w_dir", "T", "rel_H", "low_T", "low_rel_H",
             "p"]
+
+# Llegim les dades de l'arxiu
 data = pd.read_csv(arxiu, sep="\s+", engine='python', 
                    names=colnames, header=None)
+
+H = data.H
+H[H < -999] = np.nan
+
+fun.Grafiques(data.time, H, "Humitat")
+
+LE = data.LE
+LE[LE < -999] = np.nan
+fun.Grafiques(data.time, LE, "LE")
+
+## %% Humitat relativa
+q1 = fun.f_q(data['T'], data['p'], data['rel_H'])
+q2 = fun.f_q(data['low_T'], data['p'], data['low_rel_H'])
+
+plt.figure(dpi=300)
+plt.plot(data.time, q1, label="2 m")
+plt.plot(data.time, q2, label="0.26 m")
+plt.title("Humitat especifica")
+plt.legend()
+
+## 
--- funcions.py
+++ funcions.py
@@ -7,11 +7,45 @@
 @Version :   1.0
 '''
 
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
-def Grafiques(time, var):
+def Grafiques(time: pd.core.series.Series,  
+              var: pd.core.series.Series, titol: str):
 
     fig, ax = plt.subplots(figsize=(10, 8), dpi=600)
 
-    ax.plot(time, var)
+    ax.scatter(time, var)
     ax.set_xlabel("Temps (H)")
+    ax.set_title(titol, fontsize=20)
+
+
+def f_q(T, P, HR):
+
+    alfa = 18.0153**(-3) / 28.9644**(-3)
+    e_sat = 6.112 * np.exp( 17.67 +  T/(T + 243.5) )
+
+    rsat = alfa * e_sat / (P - e_sat)
+
+    q = 0.001 * HR / rsat
+
+    return q
+
+def ustar(U, k=0.4, z0=0.02, z2vent=2.15):
+
+    ustar = k*U / ( np.log(z2vent/z0) )
+
+    return ustar
+
+def TSemb(z_L, U, k=0.4, z0=0.02, z2vent=2.15, tht0=300):
+
+    ustar = k*U / ( np.log(z2vent/z0) )
+
+    L = - (tht0 * ustar**3) / (k*g*wT)
+
+    if z_L < 0:
+
+        xlmo = (1 - 19.3*z_L)**0.25
+
+
